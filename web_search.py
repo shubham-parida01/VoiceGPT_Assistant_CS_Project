79a32eff4269c98895a8b50716bdb5400516607f
Shubham-Parida01 <127040084+Shubham-Parida01@users.noreply.github.com>
2023-08-12 20:03:18 +0530

    Delete web_search.py

--- web_search.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-import urllib.parse
-
-def search_on_google(query):
-    search_url = f"https://www.google.com/search?q={urllib.parse.quote(query)}"
-    webbrowser.open_new_tab(search_url)
-
-
-
