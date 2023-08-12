e8194ff52aa2ddfd6aa4f813531ea5969e3d45b8
ashmit <89309422+ashmitbagga@users.noreply.github.com>
2023-08-12 20:27:18 +0530

    Add files via upload

---
+++ Internet_Availability.py
@@ -0,0 +1,11 @@
+import socket
+
+def check_internet_connection():
+    try:
+        # Attempt to connect to a well-known website (e.g., Google DNS server)
+        socket.create_connection(("8.8.8.8", 53), timeout=3)
+        return True
+    except socket.error:
+        return False
+
+
