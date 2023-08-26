f1c3f4013903a4f6610cfe8cb25557d35e8eca71
archelleus <133577546+archelleus@users.noreply.github.com>
2023-08-26 18:52:14 +0530

    Add files via upload

---
+++ DATABASE.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+def save_prompt(prompt_text):
+
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='bhavya13jain'
+    )
+
+    cursor = connection.cursor()
+
+    create_database_query = f"CREATE DATABASE IF NOT EXISTS COMMANDS_BY_USER"
+    cursor.execute(create_database_query)
+
+    cursor.execute(f"USE COMMANDS_BY_USER")
+
+    create_table_query = """
+        CREATE TABLE IF NOT EXISTS INPUTS_BY_USER (
+            sno INT AUTO_INCREMENT PRIMARY KEY,
+            prompt_text TEXT
+        )
+    """
+
+    cursor.execute(create_table_query)
+
+    insert_query = 'INSERT INTO INPUTS_BY_USER (prompt_text) VALUES (%s)'
+    cursor.execute(insert_query, (prompt_text,))
+    connection.commit()
+
+save_prompt("capital of india")
